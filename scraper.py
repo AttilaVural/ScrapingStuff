d07dadcc161dd6c5dca86c5060abc43ad9c135ea
mulvarp334 <att.vural@gmail.com>
2022-10-19 22:43:29 +0200

    Now pulls first header from dr.dk

--- scraper.py
+++ scraper.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get(
-	url="https://en.wikipedia.org/wiki/Web_scraping",
+	url="https://www.dr.dk/nyheder"
 )
 soup = BeautifulSoup(response.content, 'html.parser')
 
-title = soup.find(id="firstHeading")
-print(title.string)
+title = soup.find('div', class_="hydra-latest-news-page-short-news__heading").find('span', class_="dre-hyphenate-text")
+
+print(title.text)
